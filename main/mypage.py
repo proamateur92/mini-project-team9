b53e977eabe702db7c0003bc69e37f700f631e15
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-05-11 18:28:42 +0900

    id 값 전달 받기 메인페이지 -> 상세페이지 / 토큰값 없는 사용자 상세 페이지 접근 방지 #20

--- main/mypage.py
+++ main/mypage.py
@@ -10,29 +10,10 @@ blueprint = Blueprint("mypage", __name__, url_prefix="/")
 @blueprint.route('/mypage')
 def mypage():
     token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-
-        # 로그인한 아이디 가져오기
-        # id = user_info['id']
-
-        # DB로부터 플레이리스트 가져오기
-        # db.playlist.find_one({"id": id})
-
-        # {'id': id,
-        #  'playlist': {
-        # 1: ['title': title, 'album': album],
-        # 2: ['title':title, 'album': album],
-        # 3: ['title':title, 'album': album],
-        # }}
-
-        # doc = {
-        # }
-
-        return render_template('mypage.html', token=token_receive)
-    except jwt.exceptions.DecodeError:
-        return render_template('loginForm.html')
+    id = request.args.get('id')
+    if token_receive:
+        return render_template('mypage.html', token=token_receive, id=id)
+    return render_template('loginForm.html')
 
 # db에 들어온 정보들을 플레이리스트로 저장해서 넘겨줘요
 @blueprint.route('/playlist', methods=['GET'])
