4a64d6cf21177a827aae9cbb265980b569a396c1
Yudove <ydw2010@naver.com>
2022-05-12 00:47:50 +0900

    show_music 호출 에러

--- main/detail.py
+++ main/detail.py
@@ -5,7 +5,9 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/
 # 토큰 디코드
 SECRET_KEY = 'likeMusic'
 import jwt
+
 # 코딩 시작
+
 @blueprint.route('/detail')
 def detail():
     rank = request.args.get('rank')
@@ -15,6 +17,7 @@ def detail():
     cover = request.args.get('cover')
     token_receive = request.cookies.get('mytoken')
     return render_template('detail.html', token=token_receive, rank=rank, title=title[0], singer=singer, album=album, cover=cover)
+
 @blueprint.route('/review', methods=['POST'])
 def test_post():
     token_receive = request.cookies.get('mytoken')
@@ -34,33 +37,38 @@ def test_post():
         return render_template('detail.html', id=user_info["id"])
     except jwt.exceptions.DecodeError:
         return jsonify({'result':'fail', 'msg':'로그인 페이지로 이동합니다.'})
+
 @blueprint.route('/review', methods=['GET'])
 def test_get():
     review_total = list(db.review.find({}, {'_id': False}))
     return jsonify({'reviews':review_total})
+
 @blueprint.route('/detail', methods=['POST'])
-def insert():
+def get():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
         id = db.user.find_one({"id": payload['id']})['id']
         cover_receive = request.form['cover_give']
+        title_receive = request.form['title_give']
         album_receive = request.form['album_give']
         singer_receive = request.form['singer_give']
 
+        db.mymusic.update_one({'index':['index']}, {'$set': {'done': 1}})
 
         # 아이디 별로 index + 1
         mymusic_index = list(db.mymusic.find({'id':id}))
         count = len(mymusic_index) + 1
 
         doc = {
-
             'id': id,
             'index': count,
             'cover': cover_receive,
+            'title': title_receive,
             'album': album_receive,
-            'singer': singer_receive
+            'singer': singer_receive,
+            'done': 0
         }
         db.mymusic.insert_one(doc)
 
@@ -68,3 +76,35 @@ def insert():
         return render_template('detail.html', id=id)
     except jwt.exceptions.DecodeError:
         return jsonify({'result':'fail', 'msg':'로그인 페이지로 이동합니다.'})
+
+
+@blueprint.route('/detail', methods=['POST'])
+def remove():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        id = db.user.find_one({"id": payload['id']})['id']
+        cover_receive = request.form['cover_give']
+        title_receive = request.form['title_give']
+        album_receive = request.form['album_give']
+        singer_receive = request.form['singer_give']
+        index_receive = list(db.mymusic.find({'index'}))
+
+        db.mymusic.update_one({'index': index_receive}, {'$set': {'done': 0}})
+        db.mymusic.delete_one({'cover_give': cover_receive})
+        db.mymusic.delete_one({'title_give': title_receive})
+        db.mymusic.delete_one({'album_give': album_receive})
+        db.mymusic.delete_one({'singer_give': singer_receive})
+        # 아이디 별로 index + 1
+
+
+        return render_template('detail.html', id=id)
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 페이지로 이동합니다.'})
+
+@blueprint.route('/review', methods=['GET'])
+def music_get():
+
+    mymusic_list = list(db.mymusic.find({}, {'_id': False}))
+    return jsonify({'musics' : mymusic_list})
